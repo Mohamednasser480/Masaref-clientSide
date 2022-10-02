0084f58c99276e730dc11597cdbd9c01522429ab
User <mnasser480@gmail.com>
2022-10-02 03:02:25 +0200

    refactor/medicationClasses: update Displayed medication classes data

--- src/components/MedicationsClass.js
+++ src/components/MedicationsClass.js
@@ -1,7 +1,19 @@
 function MedicationsClass(props) {
+
   return <>
     <h1>{props.classData.title}</h1>
-    {Object.keys( props.classData.items[0]).map(classRecord=> <li>{classRecord}</li>)}
+    <ol>{
+      Object.keys( props.classData.items[0]).map(classRecord=> <li>
+        {classRecord}
+            {props.classData.items[0][classRecord].map(subRecord=> <ul>
+                <li> name: {subRecord.name}</li>
+                <li>dose: {subRecord.dose || 'Unknown'}</li>
+                <li>strength: {subRecord.strength}</li>
+            </ul>
+            )}
+      </li>)}
+    </ol>
+
   </>
 }
 export default MedicationsClass;
